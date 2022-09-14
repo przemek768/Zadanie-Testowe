dda27d0fa167d1caf61e712bb37d25555340ff12
przemek768 <przemek76816@gmail.com>
2022-09-14 15:35:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+function main($n, $weight, $first, $end, $cycle, $result){
+    for($i = 0;)
+    for($i = 0; $i < $n; $i++){
+        for($j = 0; $j < $n; $j++){
+            if($first[$i] != $end[$j]){
+                
+            }
+        }
+    }print $result;
+}
+
+$n = 6;
+$weight = [2400, 2000, 1200, 2400, 1600, 4000];
+$first = [1, 4 ,5, 3, 6, 2];
+$end = [5, 3, 2, 4, 6, 1];
+$cycle = [];
+$result = 0;
+
+main($n, $weight, $first, $end, $cycle, $result);
+
+?>
